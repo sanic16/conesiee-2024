464122436b9a09397c1e16a936c707237cac9f2f
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-20 06:31:33 -0600

    nosotros

--- src/app/nosotros/page.tsx
+++ src/app/nosotros/page.tsx
@@ -1,7 +1,87 @@
-import React from "react";
-
+import aboutImg1 from "@/../public/images/about_img_1.jpeg";
+import aboutImg2 from "@/../public/images/about_img_2.jpeg";
+import aboutImg3 from "@/../public/images/about_img_3.jpeg";
+import SimpleHeading from "@/components/simpleHeading/SimpleHeading";
+import Image from "next/image";
+import classes from "./page.module.css";
 const page = () => {
-  return <div>page</div>;
+  const objectives = [
+    {
+      id: 1,
+      title: "Académico",
+      desc: "Enriquecer el conocimiento de los estudiantes y aficionados a la tecnología, ciencia y otras ramas.",
+    },
+    {
+      id: 2,
+      title: "Social",
+      desc: "Generar convivencia entre estudiantes de distintas escuelas, facultades, universidades y de otras áreas en favor de la ingeniería.",
+    },
+    {
+      id: 3,
+      title: "Laboral",
+      desc: "Enriquecer el conocimiento de los estudiantes y aficionados a la tecnología, ciencia y otras ramas.",
+    },
+  ];
+  return (
+    <div className={`container ${classes.container}`}>
+      <div>
+        <h1 className={classes.title}>Nosotros</h1>
+        <p className={classes.description}>
+          Somos una organización sin fines de lucro que busca fomentar el
+          desarrollo de la ingeniería eléctrica y electrónica en Guatemala.
+        </p>
+      </div>
+      <div className={classes.box}>
+        <SimpleHeading
+          title="Misión"
+          description="Realizar un congreso en el cual los participantes amplíen sus conocimientos e
+interés en pro del medio ambiente y desarrollo tecnológico."
+          className={classes.simpleHeading}
+        />
+        <div className={classes.img__wrapper}>
+          <Image
+            src={aboutImg1}
+            alt="about image 3"
+            width={aboutImg1.width / 2}
+            height={aboutImg1.height / 2}
+          />
+        </div>
+      </div>
+
+      <div className={classes.box}>
+        <SimpleHeading
+          title="Visión"
+          description="Excelencia como comité organizador de congresos de la escuela de
+ingeniería mecánica eléctrica.
+"
+          className={classes.simpleHeading}
+        />
+        <div className={classes.img__wrapper}>
+          <Image
+            src={aboutImg2}
+            alt="about image 2"
+            width={aboutImg2.width / 2}
+            height={aboutImg2.height / 2}
+          />
+        </div>
+      </div>
+      <div className={classes.box}>
+        <SimpleHeading
+          title="Objetivos"
+          description={objectives}
+          className={classes.simpleHeading}
+        />
+        <div className={classes.img__wrapper}>
+          <Image
+            src={aboutImg3}
+            alt="about image 3"
+            width={aboutImg3.width / 2}
+            height={aboutImg3.height / 2}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
---
+++ src/components/simpleHeading/SimpleHeading.tsx
@@ -0,0 +1,42 @@
+import classes from "./simpleHeading.module.css";
+
+interface SimpleHeadingProps {
+  title: string;
+  description: string | { id: number; title: string; desc: string }[];
+  className: string;
+}
+
+const SimpleHeading: React.FC<SimpleHeadingProps> = ({
+  title,
+  description,
+  className,
+}) => {
+  const isDescriptionArray = Array.isArray(description);
+  let content = null;
+  if (!isDescriptionArray) {
+    content = <p className={classes.description}>{description}</p>;
+  } else {
+    content = (
+      <ul className={classes.description__list}>
+        {description.map((item) => (
+          <li key={item.id} className={classes.description__item}>
+            <h3>{item.title}</h3>
+            <p>{item.desc}</p>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <div className={className}>
+      <div>
+        <h2 className={classes.title}>{title}</h2>
+        <div className={classes.heading__line} />
+        {content}
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default SimpleHeading;
