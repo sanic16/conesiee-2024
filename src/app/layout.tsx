a35282cb530a1afea3c6634af3997225e1261d90
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-20 18:51:39 -0600

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/sections/navbar/Navbar";
+import Footer from "@/sections/footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/sections/footer/Footer.tsx
@@ -0,0 +1,50 @@
+import Link from "next/link";
+import classes from "./footer.module.css";
+import Image from "next/image";
+import logo from "@/../public/images/logo.png";
+
+const Footer = () => {
+  return (
+    <footer className={`container ${classes.container}`}>
+      <div className={classes.footer__box}>
+        <div className={classes.footer__left}>
+          <div className={classes.logo}>
+            <Image
+              src={logo}
+              width={logo.width / 10}
+              height={logo.height / 10}
+              alt="Logo CONESIEE 2024"
+            />
+          </div>
+          <ul className={classes.footer__menu}>
+            <li>
+              <Link href="/">Inicio</Link>
+            </li>
+            <li>
+              <Link href="/nosotros">Nosotros</Link>
+            </li>
+            <li>
+              <Link href="/contacto">Contacto</Link>
+            </li>
+            <li>
+              <Link href="/patrocinadores">Patrocinadores</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.footer__right}>
+          Diseñado y desarrollado por{" "}
+          <a
+            href="https://www.juliosanic.site"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            Julio Sanic
+          </a>{" "}
+          en <a href="https://www.codielectro.com">Codielectro</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
