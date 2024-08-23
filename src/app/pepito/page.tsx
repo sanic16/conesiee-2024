1d5034af165790dc05bd4d29aa73ec8f6e0ca4cb
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-22 18:07:17 -0600

    Image cloudfront

--- src/app/pepito/page.tsx
+++ src/app/pepito/page.tsx
@@ -1,11 +1,14 @@
+import classes from "./page.module.css";
 import Image from "next/image";
 
 const page = () => {
   const imageLink =
     "https://conesiee-static.codielectro.com/visitas/horus/WhatsApp%20Image%202024-08-20%20at%207.35.33%20AM.jpeg";
   return (
-    <div>
-      <Image src={imageLink} alt="Horus" fill />
+    <div className="container">
+      <div className={classes.image}>
+        <Image src={imageLink} alt="Horus" fill />
+      </div>
     </div>
   );
 };
