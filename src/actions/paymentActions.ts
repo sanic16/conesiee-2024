8e9873a518b593ab411c7dfd72970ab91be52d1d
sanic16 <julio.sanic.gt.256@gmail.com>
2024-08-25 20:56:40 +0000

    revalidation paths

--- src/actions/paymentActions.ts
+++ src/actions/paymentActions.ts
@@ -5,6 +5,7 @@ import { redirect } from "next/navigation";
 import { z } from "zod";
 import { uploadObject } from "@/utils/aws";
 import { cards } from "@/data/payment";
+import { revalidatePath } from "next/cache";
 
 const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
 
@@ -185,6 +186,7 @@ export async function createIndividualPaymentAction(
     };
   }
 
+  revalidatePath("/admin/comprobantes");
   redirect("/");
 
   return {
@@ -192,182 +194,183 @@ export async function createIndividualPaymentAction(
   };
 }
 
-const validateSingleStudentPaymentSchema = z.object({
-  name: z
-    .string()
-    .min(5, { message: "El nombre debe tener al menos 5 caracteres" })
-    .max(100, {
-      message: "El nombre debe tener menos de 100 caracteres",
-    }),
-  email: z.string().regex(/^[0-9]{13}@ingenieria\.usac\.edu\.gt$/, {
-    message: "Correo inválido, formato: 2320556340103@ingenieria.usac.edu.gt",
-  }),
-  studentId: z
-    .string()
-    .min(9, { message: "Carné inválido, formato: 201900000" })
-    .max(9, { message: "Carné inválido, formato: 201900000" }),
-  dpi: z
-    .string()
-    .min(13, { message: "DPI inválido, formato: 2320556340103" })
-    .max(13, { message: "DPI inválido, formato: 2320556340103" }),
-  phone: z.string().min(8, { message: "Número de teléfono inválido" }),
-  career: z.string().min(1, { message: "Carrera inválida" }),
-  registrationPackage: z.string().min(1, { message: "Paquete inválido" }),
-  ticketNumber: z.string().min(1, { message: "Número de boleto inválido" }),
-});
+// const validateSingleStudentPaymentSchema = z.object({
+//   name: z
+//     .string()
+//     .min(5, { message: "El nombre debe tener al menos 5 caracteres" })
+//     .max(100, {
+//       message: "El nombre debe tener menos de 100 caracteres",
+//     }),
+//   email: z.string().regex(/^[0-9]{13}@ingenieria\.usac\.edu\.gt$/, {
+//     message: "Correo inválido, formato: 2320556340103@ingenieria.usac.edu.gt",
+//   }),
+//   studentId: z
+//     .string()
+//     .min(9, { message: "Carné inválido, formato: 201900000" })
+//     .max(9, { message: "Carné inválido, formato: 201900000" }),
+//   dpi: z
+//     .string()
+//     .min(13, { message: "DPI inválido, formato: 2320556340103" })
+//     .max(13, { message: "DPI inválido, formato: 2320556340103" }),
+//   phone: z.string().min(8, { message: "Número de teléfono inválido" }),
+//   career: z.string().min(1, { message: "Carrera inválida" }),
+//   registrationPackage: z.string().min(1, { message: "Paquete inválido" }),
+//   ticketNumber: z.string().min(1, { message: "Número de boleto inválido" }),
+// });
 
-interface CreateStudentPaymentState {
-  errors: {
-    name?: string[];
-    email?: string[];
-    studentId?: string[];
-    dpi?: string[];
-    phone?: string[];
-    career?: string[];
-    registrationPackage?: string[];
-    ticketNumber?: string[];
-    paymentProofImage?: string[];
-    _form?: string[];
-  };
-}
+// interface CreateStudentPaymentState {
+//   errors: {
+//     name?: string[];
+//     email?: string[];
+//     studentId?: string[];
+//     dpi?: string[];
+//     phone?: string[];
+//     career?: string[];
+//     registrationPackage?: string[];
+//     ticketNumber?: string[];
+//     paymentProofImage?: string[];
+//     _form?: string[];
+//   };
+// }
 
-export async function createSingleStudentPaymentAction(
-  { captcha }: { captcha: string | null },
-  
-  formState: CreateStudentPaymentState,
-  formData: FormData
-): Promise<CreateStudentPaymentState> {
-  const name = formData.get("name") as string;
-  const email = formData.get("email") as string;
-  const studentId = formData.get("studentId") as string;
-  const dpi = formData.get("dpi") as string;
-  const phone = formData.get("phone") as string;
-  const career = formData.get("career") as string;
-  const registrationPackage = formData.get("registrationPackage") as string;
-  const ticketNumber = formData.get("ticketNumber") as string;
-  const paymentProofImage = formData.get("paymentProof") as File;
+// export async function createSingleStudentPaymentAction(
+//   { captcha }: { captcha: string | null },
 
-  const result = validateSingleStudentPaymentSchema.safeParse({
-    name,
-    email,
-    studentId,
-    dpi,
-    phone,
-    career,
-    registrationPackage,
-    ticketNumber,
-  });
+//   formState: CreateStudentPaymentState,
+//   formData: FormData
+// ): Promise<CreateStudentPaymentState> {
+//   const name = formData.get("name") as string;
+//   const email = formData.get("email") as string;
+//   const studentId = formData.get("studentId") as string;
+//   const dpi = formData.get("dpi") as string;
+//   const phone = formData.get("phone") as string;
+//   const career = formData.get("career") as string;
+//   const registrationPackage = formData.get("registrationPackage") as string;
+//   const ticketNumber = formData.get("ticketNumber") as string;
+//   const paymentProofImage = formData.get("paymentProof") as File;
 
-  // Validate form data
-  if (!result.success) {
-    return {
-      errors: result.error.flatten().fieldErrors,
-    };
-  }
+//   const result = validateSingleStudentPaymentSchema.safeParse({
+//     name,
+//     email,
+//     studentId,
+//     dpi,
+//     phone,
+//     career,
+//     registrationPackage,
+//     ticketNumber,
+//   });
 
-  // Validate image
-  if (!paymentProofImage || paymentProofImage.size === 0) {
-    return {
-      errors: {
-        paymentProofImage: ["Debe subir una imagen del comprobante de pago"],
-      },
-    };
-  }
+//   // Validate form data
+//   if (!result.success) {
+//     return {
+//       errors: result.error.flatten().fieldErrors,
+//     };
+//   }
 
-  // Validate image type
-  if (!allowedImageTypes.includes(paymentProofImage.type)) {
-    return {
-      errors: {
-        paymentProofImage: ["Tipo de imagen no permitido"],
-      },
-    };
-  }
-  console.log(captcha);
-  // Validate captcha
-  if (captcha === null) {
-    return {
-      errors: {
-        _form: ["Por favor, verifica que no eres un robot"],
-      },
-    };
-  }
+//   // Validate image
+//   if (!paymentProofImage || paymentProofImage.size === 0) {
+//     return {
+//       errors: {
+//         paymentProofImage: ["Debe subir una imagen del comprobante de pago"],
+//       },
+//     };
+//   }
 
-  const captchaResponse = await fetch(
-    `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SITE_SECRET}&response=${captcha}`
-  );
+//   // Validate image type
+//   if (!allowedImageTypes.includes(paymentProofImage.type)) {
+//     return {
+//       errors: {
+//         paymentProofImage: ["Tipo de imagen no permitido"],
+//       },
+//     };
+//   }
+//   console.log(captcha);
+//   // Validate captcha
+//   if (captcha === null) {
+//     return {
+//       errors: {
+//         _form: ["Por favor, verifica que no eres un robot"],
+//       },
+//     };
+//   }
 
-  const captchaData = await captchaResponse.json();
+//   const captchaResponse = await fetch(
+//     `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.RECAPTCHA_SITE_SECRET}&response=${captcha}`
+//   );
 
-  if (!captchaData.success) {
-    return {
-      errors: {
-        _form: ["Captcha inválido"],
-      },
-    };
-  }
+//   const captchaData = await captchaResponse.json();
 
-  const fileBuffer = await paymentProofImage.arrayBuffer();
-  const buffer = Buffer.from(fileBuffer);
+//   if (!captchaData.success) {
+//     return {
+//       errors: {
+//         _form: ["Captcha inválido"],
+//       },
+//     };
+//   }
 
-  const imageName = `vouchers/${studentId}-${Date.now()}-comprobante.${
-    paymentProofImage.type.split("/")[1]
-  }`;
+//   const fileBuffer = await paymentProofImage.arrayBuffer();
+//   const buffer = Buffer.from(fileBuffer);
 
-  // Upload Image
-  try {
-    await uploadObject(buffer, imageName, paymentProofImage.type);
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return {
-        errors: {
-          _form: [error.message],
-        },
-      };
-    }
-    return {
-      errors: {
-        _form: ["Error al subir la imagen, intente de nuevo más tarde"],
-      },
-    };
-  }
+//   const imageName = `vouchers/${studentId}-${Date.now()}-comprobante.${
+//     paymentProofImage.type.split("/")[1]
+//   }`;
 
-  // Save student payment
-  try {
-    await prisma.studentPayment.create({
-      data: {
-        email,
-        name,
-        studentId,
-        dpi,
-        phone,
-        career,
-        registrationPackage,
-        ticketNumber,
-        imageUrl: `https://conesiee-static.codielectro.com/${imageName}`,
-      },
-    });
-  } catch (error: unknown) {
-    if (error instanceof Error) {
-      return {
-        errors: {
-          _form: [error.message],
-        },
-      };
-    }
+//   // Upload Image
+//   try {
+//     await uploadObject(buffer, imageName, paymentProofImage.type);
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       return {
+//         errors: {
+//           _form: [error.message],
+//         },
+//       };
+//     }
+//     return {
+//       errors: {
+//         _form: ["Error al subir la imagen, intente de nuevo más tarde"],
+//       },
+//     };
+//   }
 
-    return {
-      errors: {
-        _form: ["Error al guardar el pago, intente de nuevo más tarde"],
-      },
-    };
-  }
+//   // Save student payment
+//   try {
+//     await prisma.studentPayment.create({
+//       data: {
+//         email,
+//         name,
+//         studentId,
+//         dpi,
+//         phone,
+//         career,
+//         registrationPackage,
+//         ticketNumber,
+//         imageUrl: `https://conesiee-static.codielectro.com/${imageName}`,
+//       },
+//     });
+//   } catch (error: unknown) {
+//     if (error instanceof Error) {
+//       return {
+//         errors: {
+//           _form: [error.message],
+//         },
+//       };
+//     }
 
-  redirect("/");
+//     return {
+//       errors: {
+//         _form: ["Error al guardar el pago, intente de nuevo más tarde"],
+//       },
+//     };
+//   }
 
-  return {
-    errors: {},
-  };
-}
+//   revalidatePath("/admin/comprobantes");
+//   redirect("/");
+
+//   return {
+//     errors: {},
+//   };
+// }
 
 const validateComboPaymentSchema = z.object({
   namePerson1: z
@@ -532,19 +535,25 @@ export async function createComboPaymentAction(
   if (!paymentProofImagePerson1 || paymentProofImagePerson1.size === 0) {
     return {
       errors: {
-        paymentProofImagePerson1: ["Debe subir una imagen del comprobante de pago"],
+        paymentProofImagePerson1: [
+          "Debe subir una imagen del comprobante de pago",
+        ],
       },
     };
-  }else if(!paymentProofImagePerson2 || paymentProofImagePerson2.size === 0){
+  } else if (!paymentProofImagePerson2 || paymentProofImagePerson2.size === 0) {
     return {
       errors: {
-        paymentProofImagePerson2: ["Debe subir una imagen del comprobante de pago"],
+        paymentProofImagePerson2: [
+          "Debe subir una imagen del comprobante de pago",
+        ],
       },
     };
-  }else if(!paymentProofImagePerson3 || paymentProofImagePerson3.size === 0){
+  } else if (!paymentProofImagePerson3 || paymentProofImagePerson3.size === 0) {
     return {
       errors: {
-        paymentProofImagePerson3: ["Debe subir una imagen del comprobante de pago"],
+        paymentProofImagePerson3: [
+          "Debe subir una imagen del comprobante de pago",
+        ],
       },
     };
   }
@@ -621,8 +630,16 @@ export async function createComboPaymentAction(
   // Upload Image
   try {
     await uploadObject(buffer, imageNamePerson1, paymentProofImagePerson1.type);
-    await uploadObject(buffer2, imageNamePerson2, paymentProofImagePerson2.type);
-    await uploadObject(buffer3, imageNamePerson3, paymentProofImagePerson3.type);
+    await uploadObject(
+      buffer2,
+      imageNamePerson2,
+      paymentProofImagePerson2.type
+    );
+    await uploadObject(
+      buffer3,
+      imageNamePerson3,
+      paymentProofImagePerson3.type
+    );
   } catch (error: unknown) {
     if (error instanceof Error) {
       return {
@@ -648,7 +665,7 @@ export async function createComboPaymentAction(
         dpi: dpiPerson1,
         phone: phonePerson1,
         career: careerPerson1,
-        registrationPackage: registrationPackage, 
+        registrationPackage: registrationPackage,
         ticketNumber: ticketNumberPerson1,
         imageUrl: `https://conesiee-static.codielectro.com/${imageNamePerson1}`,
       },
@@ -665,8 +682,8 @@ export async function createComboPaymentAction(
         registrationPackage: registrationPackage,
         ticketNumber: ticketNumberPerson2,
         imageUrl: `https://conesiee-static.codielectro.com/${imageNamePerson2}`,
-      }
-      });
+      },
+    });
 
     await prisma.studentPayment.create({
       data: {
@@ -679,10 +696,8 @@ export async function createComboPaymentAction(
         registrationPackage: registrationPackage,
         ticketNumber: ticketNumberPerson3,
         imageUrl: `https://conesiee-static.codielectro.com/${imageNamePerson3}`,
-      }
+      },
     });
-
-
   } catch (error: unknown) {
     if (error instanceof Error) {
       return {
@@ -699,6 +714,7 @@ export async function createComboPaymentAction(
     };
   }
 
+  revalidatePath("/admin/comprobantes");
   redirect("/");
 
   return {
