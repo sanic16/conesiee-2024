b771e23bc2b67145587a9cfbde7fd2fc83c2ba6b
sanic16 <julio.sanic.gt.256@gmail.com>
2024-08-25 17:05:22 +0000

    nested routes middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,7 +21,9 @@ export default auth((req) => {
   console.log("NEXT URL: ", nextUrl.pathname);
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isPublicRoute = publicRoutes.some((route) =>
+    nextUrl.pathname.startsWith(route)
+  );
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
   console.log("IS API AUTH ROUTE: ", isApiAuthRoute);
