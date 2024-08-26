b87c78399c15513969bf053f63b86ab717324422
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-26 16:50:15 -0600

    admin vouchers static - dynamic page

--- src/app/admin/comprobantes/page.tsx
+++ src/app/admin/comprobantes/page.tsx
@@ -4,6 +4,8 @@ import classes from "./page.module.css";
 import PageHeading from "@/components/pageHeading/PageHeading";
 import prisma from "@/lib/prisma";
 
+export const dynamic = "force-dynamic";
+
 export default async function ComprobantesPage() {
   const payments = await prisma.studentPayment.findMany({});
   return (
