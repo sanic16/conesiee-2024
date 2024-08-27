29f5e4c5d1a1c2109c86d404994ca7e2b00616dc
Julio Sanic <julio.sanic.gt.256@gmail.com>
2024-08-26 22:10:32 -0600

    payment cards

--- src/app/inscripciones/Info.tsx
+++ src/app/inscripciones/Info.tsx
@@ -1,4 +1,4 @@
-import { cards } from "@/data/payment";
+import { paymentOptions } from "@/data/payment";
 import classes from "./info.module.css";
 import Link from "next/link";
 
@@ -10,31 +10,34 @@ const Info = () => {
     <div className={classes.info__container}>
       {/* <HomeHeading title="Información de inscripción" description="" /> */}
       <div className={classes.info}>
-        {cards.map((card) => {
+        {paymentOptions.map((option) => {
           return (
             <div
-              key={card.id}
+              key={option.id}
               className={`${classes.card} ${
-                goldOptionId === card.id ? classes.gold : ""
-              } ${silverOptionId === card.id ? classes.silver : ""} ${
-                bronzeOptionId === card.id ? classes.bronze : ""
+                goldOptionId === option.id ? classes.gold : ""
+              } ${silverOptionId === option.id ? classes.silver : ""} ${
+                bronzeOptionId === option.id ? classes.bronze : ""
               }`}
             >
               <div className={classes.card__bg}>
                 <div className={classes.content}>
-                  <h3>{card.title}</h3>
-                  <p>{card.description}</p>
-                  <p>
-                    <strong>Precio:</strong> {card.price}
-                  </p>
-                  <Link
-                    href={card.link}
-                    className="btn primary"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Inscribirse
-                  </Link>
+                  <h3>{option.title}</h3>
+                  <div className={classes.card__footer}>
+                    <div className={classes.card__btn}>
+                      <Link
+                        href={option.link}
+                        className="btn primary"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        Inscribirse
+                      </Link>
+                    </div>
+                    <div className={classes.card__icon}>
+                      <option.icon />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
--- src/data/payment.ts
+++ src/data/payment.ts
@@ -1,59 +1,74 @@
-export const cards = [
+import { IconType } from "react-icons";
+import { FaUserGraduate } from "react-icons/fa";
+import { FaUsers } from "react-icons/fa";
+import { FaUserTie } from "react-icons/fa";
+
+interface PaymentOption {
+  id: number;
+  title: string;
+  link: string;
+  icon: IconType;
+}
+
+export const paymentOptions = [
   {
     id: 1,
-    title: "Estudiantes USAC Completo",
-    description: "Con Cena de Gala",
-    price: "Q500",
+    title: "Inscripción Individual USAC",
+    // description: "Con Cena de Gala",
+    // price: "Q500",
     link: "https://forms.gle/SyLLjsCDJA3nLDw96",
+    icon: FaUserGraduate,
   },
   {
     id: 2,
-    title: "Estudiantes USAC Simple",
-    description: "Sin Cena de Gala",
-    price: "Q400",
+    title: "Combo Cuates USAC/Externos",
+    // description: "Sin Cena de Gala",
+    // price: "Q400",
     link: "https://forms.gle/SyLLjsCDJA3nLDw96",
+    icon: FaUsers,
   },
   {
     id: 3,
-    title: "Combo Estudiantes USAC",
-    description: "3 personas + Cena de Gala",
-    price: "Q1350",
+    title: "Inscripción Externos/Profesionales",
+    // description: "3 personas + Cena de Gala",
+    // price: "Q1350",
     link: "https://forms.gle/SbgJ7DsWCzsaS6mb7",
+    icon: FaUserTie,
   },
-  {
-    id: 4,
-    title: "Day Pass USAC",
-    description: "Solo un día de actividades",
-    price: "Q200",
-    link: "https://forms.gle/SyLLjsCDJA3nLDw96",
-  },
-  {
-    id: 5,
-    title: "Externos Completo",
-    description: "Con Cena de Gala",
-    price: "Q600",
-    link: "https://forms.gle/ZF8Trnx1GADvx7Gz6",
-  },
-  {
-    id: 6,
-    title: "Externos Simple",
-    description: "Sin Cena de Gala",
-    price: "Q500",
-    link: "https://forms.gle/ZF8Trnx1GADvx7Gz6",
-  },
+  // {
+  //   id: 4,
+  //   title: "Day Pass USAC",
+  //   description: "Solo un día de actividades",
+  //   price: "Q200",
+  //   link: "https://forms.gle/SyLLjsCDJA3nLDw96",
+  // },
+  // {
+  //   id: 5,
+  //   title: "Externos Completo",
+  //   description: "Con Cena de Gala",
+  //   price: "Q600",
+  //   link: "https://forms.gle/ZF8Trnx1GADvx7Gz6",
+  // },
+  // {
+  //   id: 6,
+  //   title: "Externos Simple",
+  //   description: "Sin Cena de Gala",
+  //   price: "Q500",
+  //   link: "https://forms.gle/ZF8Trnx1GADvx7Gz6",
+  // },
 
-  {
-    id: 7,
-    title: "Combo Externos",
-    description: "3 personas + Cena de Gala",
-    price: "Q1650",
-    link: "https://forms.gle/SbgJ7DsWCzsaS6mb7",
-  },
-  {
-    id: 8,
-    title: "Day Pass Externos",
-    description: "Solo un día de actividades",
-    price: "Q250",
-    link: "https://forms.gle/SbgJ7DsWCzsaS6mb7",
-  },
+  // {
+  //   id: 7,
+  //   title: "Combo Externos",
+  //   description: "3 personas + Cena de Gala",
+  //   price: "Q1650",
+  //   link: "https://forms.gle/SbgJ7DsWCzsaS6mb7",
+  // },
+  // {
+  //   id: 8,
+  //   title: "Day Pass Externos",
+  //   description: "Solo un día de actividades",
+  //   price: "Q250",
+  //   link: "https://forms.gle/SbgJ7DsWCzsaS6mb7",
+  // },
 ];
